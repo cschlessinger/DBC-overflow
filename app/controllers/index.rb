4e1b8c050163cf7142b9c073fca1461228ce58d4
Grant Wong <grantmwong@gmail.com>
2015-10-08 16:06:36 -0700

    Question voting up down

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,19 +1,11 @@
 get '/questions' do
   @questions = Question.all
-
   erb :question_list
 end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :question
-end
-
-post '/questions/:question_id/answers/:answer_id/upvote' do
-  answer = Answer.find(params[:answer_id])
-  answer.scores.create(value: 1)
-  score = answer.scores.count
-  score.to_s
 end
 
 get '/question/new' do
@@ -24,6 +16,27 @@ end
 post '/questions' do
   Question.create(params[:question])
   redirect 'questions'
+end
+
+post '/questions/:question_id/upvote' do
+  question = Question.find(params[:question_id])
+  question.scores.create(value: 1)
+  score = question.scores.count
+  score.to_s
+end
+
+post '/questions/:question_id/downvote' do
+  question = Question.find(params[:question_id])
+  question.scores.last.destroy
+  score = question.scores.count
+  score.to_s
+end
+
+post '/questions/:question_id/answers/:answer_id/upvote' do
+  answer = Answer.find(params[:answer_id])
+  answer.scores.create(value: 1)
+  score = answer.scores.count
+  score.to_s
 end
 
 post '/questions/:question_id/answers/:answer_id/downvote' do
